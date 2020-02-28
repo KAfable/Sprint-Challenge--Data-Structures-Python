b2cf8e7af48c77115e11485afd79ea8944a7476c
Vyraal1 <kevin.afable@gmail.com>
2020-02-28 12:40:00 -0500

    feat: BST, Dict, and Set solutions

--- names/names.py
+++ names/names.py
@@ -1,6 +1,6 @@
 import time
+from binary_search_tree import BinarySearchTree
 
-start_time = time.time()
 
 f = open('names_1.txt', 'r')
 names_1 = f.read().split("\n")  # List containing 10000 names
@@ -10,23 +10,42 @@ f = open('names_2.txt', 'r')
 names_2 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
-duplicates = []  # Return the list of duplicates in this data structure
-
 # Replace the nested for loops below with your improvements
+bst_duplicates = []
+bst_start_time = time.time()
 
-# can't we just use a dictionary? why do we have to use a BST?
+name_tree = BinarySearchTree('fake name')
+for name in names_1:
+    name_tree.insert(name)
+
+for name in names_2:
+    if name_tree.contains_iteratively(name):
+        bst_duplicates.append(name)
+
+bst_end_time = time.time()
+print(f"{len(bst_duplicates)} duplicates:\n\n{', '.join(bst_duplicates)}\n\n")
+print(f"BST runtime: {bst_end_time - bst_start_time} seconds")
+
+# ---------- Stretch Goal -----------
+# Dictionary
+dict_duplicates = []
+dict_start_time = time.time()
+
 dict1 = {name: 1 for name in names_1}
 for name in names_2:
     if name in dict1:
-        duplicates.append(name)
+        dict_duplicates.append(name)
 
-end_time = time.time()
-print(f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
-print(f"runtime: {end_time - start_time} seconds")
+dict_end_time = time.time()
+# print(f"{len(dict_duplicates)} duplicates:\n\n{', '.join(dict_duplicates)}\n\n")
+print(f"Dict runtime: {dict_end_time - dict_start_time} seconds")
 
-# ---------- Stretch Goal -----------
-# Python has built-in tools that allow for a very efficient approach to this problem
-# What's the best time you can accomplish?  Thare are no restrictions on techniques or data
-# structures, but you may not import any additional libraries that you did not write yourself.
+# Set
+set_start_time = time.time()
+names_1_set = set(names_1)
+names_2_set = set(names_2)
+set_duplicates = names_1_set.intersection(names_2_set)
 
-# check above
+set_end_time = time.time()
+# print(f"{len(set_duplicates)} duplicates:\n\n{', '.join(set_duplicates)}\n\n")
+print(f"Set runtime: {set_end_time - set_start_time} seconds")
