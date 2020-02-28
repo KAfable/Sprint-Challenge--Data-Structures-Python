c63fcdbbcddaac44999239ddaf7b276e7a3690fb
Vyraal1 <kevin.afable@gmail.com>
2020-02-28 13:14:59 -0500

    feat: adding ring buffer mvp

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -4,19 +4,42 @@ from doubly_linked_list import DoublyLinkedList
 class RingBuffer:
     def __init__(self, capacity):
         self.capacity = capacity
-        self.current = None
+        self.current = None  # what's going to be overwritten
         self.storage = DoublyLinkedList()
 
+    def __repr__(self):
+        return self.storage.__repr__()
+
     def append(self, item):
-        pass
+        # if empty, set the current to the head
+        if self.storage.length == 0:
+            self.storage.add_to_tail(item)
+            self.current = self.storage.head
+        # if less than capcacity, link the tail to head after adding
+        elif self.storage.length < self.capacity:
+            self.storage.add_to_tail(item)
+            self.storage.tail.next = self.storage.head
+        # otherwise, overwrite the oldest, and move the oldest to next item
+        # since tail is linked to head, this works fine
+        else:
+            self.current.value = item
+            self.current = self.current.next
+
+            # if at capacity, remove the oldest (head), add to tail
+            # you don't replace new nodes, you change the value
 
     def get(self):
         # Note:  This is the only [] allowed
         list_buffer_contents = []
+        current = self.storage.head
+        while current is not self.storage.tail:
+            list_buffer_contents.append(current.value)
+            current = current.next
 
-        # TODO: Your code here
+        list_buffer_contents.append(self.storage.tail.value)
 
         return list_buffer_contents
+
 
 # ----------------Stretch Goal-------------------
 
